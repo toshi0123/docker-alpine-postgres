dbf12ba18107e54d962ea7ea5f54d1f09c3b810a
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-10-11 01:46:46 +0900

    Reindented

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,20 +10,20 @@ if [ -z "$(ls -A "$PGDATA")" ];then
 
 	: ${POSTGRES_USER:="postgres"}
 	: ${POSTGRES_PASS:="postgres"}
-    : ${POSTGRES_DB:="postgres"}
-    : ${LISTEN_ADDRESSES:="0.0.0.0/0"}
+	: ${POSTGRES_DB:="postgres"}
+	: ${LISTEN_ADDRESSES:="0.0.0.0/0"}
 
-    if [ "$POSTGRES_DB" != 'postgres' ]; then
-	    echo "CREATE DATABASE $POSTGRES_DB;" | su-exec postgres postgres --single -jE
+	if [ "$POSTGRES_DB" != 'postgres' ]; then
+		echo "CREATE DATABASE $POSTGRES_DB;" | su-exec postgres postgres --single -jE
 	fi
 
 	if [ "$POSTGRES_USER" != 'postgres' ]; then
-	    echo "CREATE USER $POSTGRES_USER WITH SUPERUSER PASSWORD '$POSTGRES_PASS';" | su-exec postgres postgres --single -jE
+		echo "CREATE USER $POSTGRES_USER WITH SUPERUSER PASSWORD '$POSTGRES_PASS';" | su-exec postgres postgres --single -jE
 	else
 		echo "ALTER USER $POSTGRES_USER WITH SUPERUSER PASSWORD '$POSTGRES_PASS';" | su-exec postgres postgres --single -jE
 	fi
 
-    { echo; echo "host all all $LISTEN_ADDRESSES md5"; } >> $PGDATA/pg_hba.conf
+	{ echo; echo "host all all $LISTEN_ADDRESSES md5"; } >> $PGDATA/pg_hba.conf
 fi
 
 mkdir -p /run/postgresql
