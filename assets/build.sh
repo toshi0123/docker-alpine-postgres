7155a80fac309c722fb184067833d6baf14fa3b3
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-12-22 13:56:23 +0900

    Create build.sh

---
+++ assets/build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+apk add --no-cache --virtual .builddev build-base lz4-dev wget ca-certificates postgresql-dev
+
+# install groonga
+wget https://packages.groonga.org/source/groonga/groonga-7.0.9.tar.gz
+tar xvzf groonga-7.0.9.tar.gz
+cd groonga-7.0.9
+./configure --with-lz4
+make -j$(nproc)
+make install
+
+cd -
+
+# install pgroonga
+wget https://packages.groonga.org/source/pgroonga/pgroonga-2.0.2.tar.gz
+tar xvf pgroonga-2.0.2.tar.gz
+cd pgroonga-2.0.2
+make -j$(nproc)
+make install
+
+cd -
+
+rm -rf groonga-7.0.9 pgroonga-2.0.2
+rm -f  groonga-7.0.9.tar.gz pgroonga-2.0.2.tar.gz
+
+# Remove build dependencies
+apk del --no-cache .builddev
